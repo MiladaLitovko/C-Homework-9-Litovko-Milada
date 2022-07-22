e90ee3c4d0a97ad130e8c100ac2a125a645c65ed
litovkomilada <litovkomiada@yandex.ru>
2022-07-22 21:31:16 +0300

    Complete task64

---
+++ Task64/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N, кратные трём.
+M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+M = 4; N = 8. -> ""4, 6, 7, 8""
+*/
+
+Zadacha64();
+
+void Zadacha64()
+{
+    Console.WriteLine("Введите число M");
+    int M = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите число N");
+    int N = Convert.ToInt32(Console.ReadLine());
+    Recursion(M, N);
+    
+    void Recursion(int M, int N)
+    {
+      if (M > N) return;
+      if(M % 3 == 0)
+      {
+        Console.Write(M + " ");
+      }
+      M++;
+      Recursion(M, N);
+    }
+}
+
